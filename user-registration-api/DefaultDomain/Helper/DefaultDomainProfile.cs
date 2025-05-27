7b0645595d837ad2a288adef9801d6046d5c7e18
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-05-27 01:59:08 -0500

    Connection Configuration and Mapper Configuration added - connection configuration added - mapper configuration added - beans registered - validators configuration added

---
+++ user-registration-api/DefaultDomain/Helper/DefaultDomainProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using user_registration_api.DefaultDomain.Dtos;
+using user_registration_api.DefaultDomain.Models;
+
+namespace user_registration_api.DefaultDomain.Helper;
+
+public class DefaultDomainProfile: Profile
+{
+    public DefaultDomainProfile()
+    {
+        CreateMap<User, User>();
+        CreateMap<User, UserDto>();
+        CreateMap<UserDto, User>();
+        CreateMap<UserSearch, UserSearchDto>();
+        CreateMap<UserSearchDto, UserSearch>();
+    }
+}
--- user-registration-api/Program.cs
+++ user-registration-api/Program.cs
@@ -1,8 +1,31 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using user_registration_api.DefaultDomain.Data;
+using user_registration_api.DefaultDomain.Repositories;
+using user_registration_api.DefaultDomain.Repositories.Impl;
+using user_registration_api.DefaultDomain.Services;
+using user_registration_api.DefaultDomain.Services.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IJsonService, JsonService>();
 
 builder.Services.AddControllers();
+
+// model validators configuration
+foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) builder.Services.AddValidatorsFromAssembly(assembly);
+
+// automapper configuration
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContextFactory<CustomDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("CustomDbContext"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
