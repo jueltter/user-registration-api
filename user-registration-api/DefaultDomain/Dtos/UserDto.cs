d992b936ff21d1960177c232686bed7de5e8bd24
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-05-27 02:00:31 -0500

    Model and Dto mapped - model mapped - dto mapped

--- user-registration-api/DefaultDomain/Dtos/UserDto.cs
+++ user-registration-api/DefaultDomain/Dtos/UserDto.cs
@@ -3,4 +3,7 @@ namespace user_registration_api.DefaultDomain.Dtos;
 public class UserDto
 {
     public long? IdUser { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public DateTime? CreatedAt { get; set; }
 }
--- user-registration-api/DefaultDomain/Models/User.cs
+++ user-registration-api/DefaultDomain/Models/User.cs
@@ -1,6 +1,24 @@
-﻿namespace user_registration_api.DefaultDomain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace user_registration_api.DefaultDomain.Models;
+
+[Table("users", Schema = "public")]
 public class User
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    [Column("id")]
     public int? IdUser { get; set; }
+    
+    [MaxLength(short.MaxValue)]
+    [Column("username")]
+    public required string Username { get; set; }
+    
+    [MaxLength(short.MaxValue)]
+    [Column("email")]
+    public required string Email { get; set; }
+    
+    [Column("created_at")]
+    public DateTime? CreatedAt { get; set; }
 }
